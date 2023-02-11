a9fd7a8cb5f2f6dee7fa78c573094c01722d7f90
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-10 22:13:49 -0500

    Updated exports

--- src/wheather/index.ts
+++ src/wheather/index.ts
@@ -1,3 +1,4 @@
+export { Wheater } from './components/wheather/Wheater';
 export { ApiWheater } from './api/ApiWheater';
 export { SearchWheater } from './components/searchWheater/SearchWheater';
 export { WheatherPages } from './pages/WheatherPages';
