1b2b0d3627578ba65893657205309ec03ef48932
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-11 22:18:25 -0500

    New reducer has been added

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { wheaterSlice } from './wheather/weatherSlice';
+import { weatherCitySlice } from './wheather/wheaterCitySlice';
 
 export const store = configureStore({
     reducer: {
-        wheater: wheaterSlice.reducer
+        wheater: wheaterSlice.reducer,
+        wheaterCity: weatherCitySlice.reducer,
     },
 });
