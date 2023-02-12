1c8b54fe64041231047af6db829d7d9e5446d66a
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-11 22:22:27 -0500

    The Api has been updated

--- src/wheather/api/ApiWheater.ts
+++ src/wheather/api/ApiWheater.ts
@@ -7,6 +7,7 @@ const { VITE_API_KEY, VITE_API_URL_WHEATER } = getEnvVariables();
 export const ApiWheater = axios.create({
     baseURL: VITE_API_URL_WHEATER,
     params: {
-        appid: VITE_API_KEY
+        appid: VITE_API_KEY,
+        lang: 'es'
     }
 });
