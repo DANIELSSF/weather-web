c7b4053e8f2e6e9e6a399a0e1a3eb8d8a47f2d2b
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-11 22:21:43 -0500

    Slice has been updated

--- src/store/wheather/wheaterCitySlice.ts
+++ src/store/wheather/wheaterCitySlice.ts
@@ -1,17 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getDefaultInfoCity } from '../../wheather/helpers/getDefaultInfoCity';
+import { WeatherCityInterface } from '../../wheather/interfaces/interfaces-Slice/WeatherCityInterface';
 
 export const weatherCitySlice = createSlice({
     name: 'weatherCity',
     initialState: {
-        counter: 10
-    },
+        isLoading: true,
+        data: getDefaultInfoCity,
+        datasCity: {},
+        isErrorCity: ''
+
+    } as WeatherCityInterface,
     reducers: {
-        increment: (state, /* action */ ) => {
-            state.counter += 1;
+        checking: (state) => {
+            state.isLoading = true;
+            state.data = getDefaultInfoCity
+            state.isErrorCity = undefined;
         },
+        onLoad: (state, { payload }) => {
+            state.isLoading = false;
+            state.datasCity = payload;
+            state.isErrorCity = undefined;
+        },
+        errorMessage: (state, { payload }) => {
+            state.isLoading = false;
+            state.data = getDefaultInfoCity;
+            state.isErrorCity = payload;
+        }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { increment } = weatherCitySlice.actions;
+export const {
+    checking,
+    errorMessage,
+    onLoad
+} = weatherCitySlice.actions;
