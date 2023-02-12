b8ddd4de4e2eca7dc9bc6025382219c0f82e8eb9
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-11 22:20:01 -0500

    Interface has been moved to another file

--- src/store/wheather/WheatherInterface.ts
+++
@@ -1,7 +0,0 @@
-import { APIWheaterInterface } from '../../wheather/interfaces/ApiWheaterInterface';
-
-export interface WheaterInterface {
-    isLoading: boolean,
-    datas: APIWheaterInterface[]
-    isError: undefined | string
-}
