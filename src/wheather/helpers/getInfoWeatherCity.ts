228918ca931383d5676ddaac2735608dff42294a
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-13 21:14:08 -0500

    Helper updated

--- src/wheather/helpers/getInfoWeatherCity.ts
+++ src/wheather/helpers/getInfoWeatherCity.ts
@@ -1,22 +1,26 @@
-import { APIWheaterInterface, Main, Weather } from '../interfaces/interfaces-Api/ApiWheaterInterface';
+import { Main, Weather } from '../interfaces/interfaces-Api/ApiWheaterInterface';
 
 export const getInfoWeatherCity = (weather: Weather[], main: Main) => {
-
+    
     if (!weather || !main) return {};
 
     const { description, icon } = weather[0];
     let { temp, temp_max, temp_min, humidity } = main;
-
     const kelvin: number = 273.15;
-    temp -= kelvin; // Convertir de Kelvin a Celsius
-    temp_max -= kelvin; // Convertir de Kelvin a Celsius
-    temp_min -= kelvin; // Convertir de Kelvin a Celsius
+
+    (!temp || !temp_max || !temp_min)
+        ? {}
+        : temp -= kelvin; // Convertir de Kelvin a Celsius
+          temp_max -= kelvin; // Convertir de Kelvin a Celsius
+          temp_min -= kelvin; // Convertir de Kelvin a Celsius
+        ;
 
     return {
         description,
         temp,
         temp_max,
         temp_min,
-        humidity
+        humidity,
+        icon
     }
 }
