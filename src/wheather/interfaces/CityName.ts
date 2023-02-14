21651f26fc6b5acf5801142b5a2f6cf8a8b6d127
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-13 21:06:04 -0500

    These files have been removed

--- src/wheather/interfaces/CityName.ts
+++
@@ -1,3 +0,0 @@
-export interface CityName{
-    cityName: string | string[] ,
-}
--- src/wheather/interfaces/interfaces-Slice/WeatherCityInterface.ts
+++
@@ -1,8 +0,0 @@
-import { APIWheaterInterface } from '../interfaces-Api/ApiWheaterInterface';
-
-export interface WeatherCityInterface {
-    isLoading: boolean,
-    data: APIWheaterInterface,
-    datasCity: APIWheaterInterface,
-    isErrorCity: string | undefined,
-}
