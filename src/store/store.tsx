139265b0e891042afea3effe1487d0cd0d533f61
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-13 21:08:55 -0500

    The store and the slice were updated to handle all the information in one slice

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { wheaterSlice } from './wheather/weatherSlice';
-import { weatherCitySlice } from './wheather/wheaterCitySlice';
 
 export const store = configureStore({
     reducer: {
         wheater: wheaterSlice.reducer,
-        wheaterCity: weatherCitySlice.reducer,
     },
 });
--- src/store/wheather/weatherSlice.ts
+++ src/store/wheather/weatherSlice.ts
@@ -1,44 +1,65 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { WheaterInterface } from '../../wheather/interfaces/interfaces-Slice/WheatherInterface';
+import { getDefaultInfoCity } from '../../wheather/helpers/getDefaultInfoCity';
+import { getDefaultInfoCoords } from '../../wheather/helpers/getDefaultInfoCoords';
 
 export const wheaterSlice = createSlice({
     name: 'wheater',
     initialState: {
         isLoading: true,
-        datas: [],
-        isError: undefined
+        isErrorCity: undefined,
+        isErrorCoords: undefined,
+        dataCoords: [],
+        dataCity: getDefaultInfoCity,
+
     } as WheaterInterface,
 
     reducers: {
-        checking: (state) => {
+        checkingCoords: (state):void => {
             state.isLoading = true;
-            state.datas = [{
-                name: '',
-                lat: 0,
-                lon: 0,
-                country: '',
-                state: ''
-            }];
-            state.isError = undefined;
+            state.dataCoords = [getDefaultInfoCoords];
+            state.isErrorCoords = undefined;
+            state.isErrorCity = undefined;
+            state.dataCity = getDefaultInfoCity;
         },
-        onLoaded: (state, { payload  }) => {
+        checkingCity: (state):void => {
+            state.isLoading = true;
+            state.dataCity = getDefaultInfoCity;
+            state.isErrorCoords = undefined;
+            state.isErrorCity = undefined;
+        },
+        onLoadedCoords: (state, { payload }):void => {
             state.isLoading = false;
-            state.datas = payload;
-            state.isError = undefined;
+            state.dataCoords = payload;
+            state.isErrorCoords = undefined;
+            state.isErrorCity = undefined;
         },
-        clearErrorMessage: (state) => {
-            state.isError = undefined;
+        onLoadedCity: (state, { payload }):void => {
+            state.isLoading = false;
+            state.dataCity = payload;
+            state.isErrorCoords = undefined;
+            state.isErrorCity = undefined;
         },
-        errorMessage: (state, { payload }) => {
-            state.isError = payload;
+        clearErrorMessage: (state):void => {
+            state.isErrorCoords = undefined;
+            state.isErrorCity = undefined;
+        },
+        errorMessageCity: (state, { payload }):void => {
+            state.isErrorCity = payload;
+        },
+        errorMessageCoords: (state, { payload }):void => {
+            state.isErrorCoords = payload;
         }
     }
 });
 
 
 export const {
-    checking,
-    onLoaded,
+    checkingCoords,
+    checkingCity,
+    onLoadedCity,
     clearErrorMessage,
-    errorMessage
+    errorMessageCoords,
+    errorMessageCity,
+    onLoadedCoords
 } = wheaterSlice.actions;
