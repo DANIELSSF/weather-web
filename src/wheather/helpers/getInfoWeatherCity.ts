dfda68b3b4d46415d61c3a3da6bc26e7f3eb6649
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-14 22:46:30 -0500

    Now get the wind speed from the API

--- src/wheather/helpers/getInfoWeatherCity.ts
+++ src/wheather/helpers/getInfoWeatherCity.ts
@@ -1,26 +1,32 @@
-import { Main, Weather } from '../interfaces/interfaces-Api/ApiWheaterInterface';
+import { Main, Weather, Wind } from '../interfaces/interfaces-Api/ApiWheaterInterface';
 
-export const getInfoWeatherCity = (weather: Weather[], main: Main) => {
-    
+export const getInfoWeatherCity = (weather: Weather[], main: Main, wind: Wind) => {
+
     if (!weather || !main) return {};
 
     const { description, icon } = weather[0];
-    let { temp, temp_max, temp_min, humidity } = main;
+    let { temp, temp_max, humidity } = main;
+    let { speed } = wind;
+
     const kelvin: number = 273.15;
 
-    (!temp || !temp_max || !temp_min)
+    (!temp || !temp_max)
         ? {}
         : temp -= kelvin; // Convertir de Kelvin a Celsius
-          temp_max -= kelvin; // Convertir de Kelvin a Celsius
-          temp_min -= kelvin; // Convertir de Kelvin a Celsius
-        ;
+    temp_max -= kelvin; // Convertir de Kelvin a Celsius
+    ;
+
+
+    (!speed)
+        ? {}
+        : speed *= 1.60934;
 
     return {
         description,
         temp,
         temp_max,
-        temp_min,
         humidity,
+        speed,
         icon
     }
 }
