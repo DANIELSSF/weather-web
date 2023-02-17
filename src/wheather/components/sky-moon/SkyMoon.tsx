41d3ecc8770171bb0d1a36ed8fd861ad9b1db017
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-16 23:25:15 -0500

    HTML code and CSS code for the animation of the moon

---
+++ src/wheather/components/sky-moon/SkyMoon.tsx
@@ -0,0 +1,15 @@
+import './skymoon.css';
+
+export const SkyMoon = () => {
+    return (
+        <>
+            <div className="SkyMoon__container">
+                <div className="SkyMoon__cloud"></div>
+                <div className="SkyMoon__cloud"></div>
+                <div className="SkyMoon__star"></div>
+                <div className="SkyMoon__moon"></div>
+                <div className="SkyMoon__shadow"></div>
+            </div>
+        </>
+    )
+}
